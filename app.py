bbfcda6adaa690aaa0b552401201fc011cee7131
marzie <marzie.7900@gmail.com>
2020-06-11 01:54:38 +0430

    first part of url for making a random word

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Api, Resource
-from urllib.parse import quote_plus
+from flask import request
 
 import string
 import random
@@ -10,19 +10,20 @@ app = Flask(__name__)
 api = Api(app)
 
 
-def random_generator(size=6, chars=string.ascii_letters):
+def random_generator(size=5, chars=string.ascii_letters):
     return ''.join(random.choice(chars) for x in range(size))
 
 
 class First(Resource):
-    def post(self, url):
-        print(url[0:7])
+    def post(self):
+        # I give a param with key url for sending data
+        url = request.args.get('url')
         if url[0:7] == 'http://' or url[0:8] == 'https://':
             return {'short_url': random_generator()}
         return {'error': 'it is not url'}
 
-#just for makinf diffrence
-api.add_resource(First, '/<string:url>')
+
+api.add_resource(First, '/')
 
 if __name__ == '__main__':
     app.run()
