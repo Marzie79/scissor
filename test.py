cb6ff24e3856f6658d027a1989eec7ee0a0b7f72
marzie <marzie.7900@gmail.com>
2020-06-17 02:36:20 +0430

    making test for validate url

--- test.py
+++ test.py
@@ -1,1 +1,43 @@
+import json
+import unittest
+from app import app
 
+app.testing = True
+
+
+class TestFlaskApiUsingRequests(unittest.TestCase):
+    def test_url_first(self):
+        response = app.test_client().post(
+            '/?url=http://adshjkl.ir'
+        )
+        data = response.get_data('url')
+        res = json.loads(data)
+        self.assertIn('short_url', res)
+
+    def test_url_second(self):
+        response = app.test_client().post(
+            '/?url=http://'
+        )
+        data = response.get_data('url')
+        res = json.loads(data)
+        self.assertIn('error', res)
+
+    def test_url_third(self):
+        response = app.test_client().post(
+            '/?url=http://k.com'
+        )
+        data = response.get_data('url')
+        res = json.loads(data)
+        self.assertIn('short_url', res)
+
+    def test_url_forth(self):
+        response = app.test_client().post(
+            '/?url=http://.com'
+        )
+        data = response.get_data('url')
+        res = json.loads(data)
+        self.assertIn('error', res)
+
+
+if __name__ == "__main__":
+    unittest.main()
