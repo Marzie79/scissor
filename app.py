858438170efae1e89ebcae8e361f488393680c83
marzie <marzie.7900@gmail.com>
2020-06-20 03:24:31 +0430

    making expire time for url

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+from datetime import datetime
+
 from flask import Flask, redirect
 from flask_restful import Api, Resource
 from flask import request
 from model import db, Url
 import string
 import random
-from urlvalidator import ValidationError, URLValidator
+from urlvalidator import URLValidator
 
 app = Flask(__name__)
 api = Api(app)
@@ -23,21 +25,27 @@ class Get_url(Resource):
     def post(self):
         try:
             long_url = request.args.get('url')
+            time = request.args.get('time')
             validate(long_url)
             short_url = random_generator()
-            url = Url(long_url, short_url)
+            url = Url()
+            if time is not None:
+                url.pub_date = datetime.strptime(time, '%Y-%m-%d %H:%M')
+            url.long_url = long_url
+            url.short_url = short_url
             db.session.add(url)
             db.session.commit()
             return {'short_url': short_url}
-        except ValidationError as exception:
-            return {'error': 'it is not url'}
+        except Exception:
+            return {'error': 'an error has occurred'}
 
 
 class Redirect(Resource):
     def get(self, url):
         exist_url = Url.query.filter_by(short_url=url)
         if exist_url is not None and exist_url.count() == 1:
-            return redirect(exist_url[0].long_url, 302)
+            if exist_url[0].pub_date is None or exist_url[0].pub_date > datetime.now():
+                return redirect(exist_url[0].long_url, 302)
         return {'error': 'an error has occurred'}
 
 
